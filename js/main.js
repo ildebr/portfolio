25c828978c808b0c81911bd667cd300ab1b1f310
ildebr <73916430+ildebr@users.noreply.github.com>
2021-08-25 14:01:00 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$("nav.about-barras").click(function(){
+    $("section.more-stuff").toggleClass("initial-pos");
+    $("img.barras").toggleClass("hide");
+    //$("section.more-stuff>h2.title").attr('data-aos', 'fade-right')
+})
+
+//$("nav.about-barras").click();
