b23a2f220e3e49afdd30d990b8c42d52a43fde54
ild <ildebrandoefrain@gmail.com>
2022-05-05 19:20:17 -0400

    uncommented

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// $("nav.about-barras").click(function(){
-//     $("section.more-stuff").toggleClass("initial-pos");
-//     $("img.barras").toggleClass("hide");
-//     ////$("section.more-stuff>h2.title").attr('data-aos', 'fade-right')
-// })
+$("nav.about-barras").click(function(){
+    $("section.more-stuff").toggleClass("initial-pos");
+    $("img.barras").toggleClass("hide");
+    ////$("section.more-stuff>h2.title").attr('data-aos', 'fade-right')
+})
 
 //$("nav.about-barras").click();
 
