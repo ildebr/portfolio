7db13db59d51ce313f35cb9e566ee5dbcdf0d73d
ild <ildebrandoefrain@gmail.com>
2022-05-05 19:19:25 -0400

    rotating animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$("nav.about-barras").click(function(){
-    $("section.more-stuff").toggleClass("initial-pos");
-    $("img.barras").toggleClass("hide");
-    //$("section.more-stuff>h2.title").attr('data-aos', 'fade-right')
-})
+// $("nav.about-barras").click(function(){
+//     $("section.more-stuff").toggleClass("initial-pos");
+//     $("img.barras").toggleClass("hide");
+//     ////$("section.more-stuff>h2.title").attr('data-aos', 'fade-right')
+// })
 
 //$("nav.about-barras").click();
+
+const text = document.querySelector('.text p');
+        text.innerHTML = text.innerText.split("").map(
+            (char,i) =>{
+                return `<span style="transform:rotate(${i * 16}deg)">${char}</span>`
+            }
+            ).join("")
+            console.log("echo")
