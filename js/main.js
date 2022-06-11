57cc022acd04b5be8c8078e1a41722e46245d441
ild <ildebrandoefrain@gmail.com>
2022-06-11 15:34:32 -0400

    updated animations

--- js/main.js
+++ js/main.js
@@ -13,3 +13,11 @@ const text = document.querySelector('.text p');
             }
             ).join("")
             console.log("echo")
+
+
+let circle = document.getElementById('circle');
+const onMouseMove = (e) =>{
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+}
+document.addEventListener('mousemove', onMouseMove);
